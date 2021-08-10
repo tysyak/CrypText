4f3af4213d9139d96566670ed753f4f1b735ce55
Cristian Romero Andrade <mascrit@gmail.com>
2021-08-10 01:04:46 -0500

    manipulación de comportamiento de BAT

---
+++ CryptoData.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import time
+import pandas as pd
+
+class CryptoData:
+    def __init__(self) -> None:
+        self.csv_list = list()
+
+    def csv_to_list(self, csv_path='tmp/bat-2020-2021.csv') -> list:
+        df = pd.read_csv(csv_path, delimiter=',')
+        self.csv_list = [list(row) for row in df.values]
+        self.csv_list.insert(0, df.columns.to_list())
+        self._time_to_timestamp()
+        return self.csv_list
+
+    def _time_to_timestamp(self):
+        for i in range(1,len(self.csv_list)):
+            self.csv_list[i][0] = time.mktime(time.strptime(self.csv_list[i][0], '%d.%m.%Y'))
+
+
+
+if __name__ == '__main__':
+    some = CryptoData()
+    print(some.csv_to_list())
